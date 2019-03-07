9b4adb589f57cf58694ca0d1692e099e52d2b7a7
Iulia Sargu <i.sargu@levi9.com>
2019-03-07 11:02:12 +0200

    Updates

---
+++ precache-manifest.58c8bfcfccb09cea10f95b3e8113d147.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a6e4adae3f8c7c8193b",
+    "url": "/random-quote-machine/static/js/runtime~main.1a6e4ada.js"
+  },
+  {
+    "revision": "605d1344c7fad9c421d2",
+    "url": "/random-quote-machine/static/js/main.605d1344.chunk.js"
+  },
+  {
+    "revision": "5d38434d1bbecf474fa5",
+    "url": "/random-quote-machine/static/js/2.5d38434d.chunk.js"
+  },
+  {
+    "revision": "605d1344c7fad9c421d2",
+    "url": "/random-quote-machine/static/css/main.00794411.chunk.css"
+  },
+  {
+    "revision": "d8c2c13111d33e6e124241d5b150ef69",
+    "url": "/random-quote-machine/index.html"
+  }
+];
